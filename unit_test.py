a8472f70aec76208a9dc2d9614b27196f0889f54
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-24 15:43:56 -0400

    Refactored Docker implementation / Updated `unit_test.py`

--- unit_test.py
+++ unit_test.py
@@ -1,12 +1,16 @@
+from os import environ
 from unittest import TestCase, main
 
 from requests import get
+
+host = environ.get('HOST')
+port = environ.get('PORT')
 
 
 class MyTestCase(TestCase):
     # Test the status of the WSGI web server
     def test_connection(self):
-        r = get('http://localhost:8000')
+        r = get(f'http://{host}:{port}')
         self.assertTrue(r.status_code == 200)
         print('HTTP 200 test -> PASSED' + '\n')
 
