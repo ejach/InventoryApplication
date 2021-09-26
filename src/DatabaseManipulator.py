7cfb85fb56753d41523c33e496de96d552a115bb
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-26 19:53:31 -0400

    Fixed insert statement / Update `DatabaseConnector.py` / Refactoring

--- src/DatabaseManipulator.py
+++ src/DatabaseManipulator.py
@@ -7,7 +7,19 @@ class DatabaseManipulator:
         self.cursor = self.db.cursor
 
     def fetchall(self):
-        stmt = 'SELECT * FROM parts'
-        self.cursor.execute(stmt)
-        results = self.cursor.fetchall()
-        return results
+        try:
+            stmt = 'SELECT * FROM parts'
+            self.cursor.execute(stmt)
+            results = self.cursor.fetchall()
+            error = ''
+            return results, error
+        except:
+            self.db.database.rollback()
+            results = None
+            error = "Error connecting to the database"
+            return results, error
+
+    def insert(self, part_name, part_number):
+        stmt = 'INSERT INTO parts (name, part_number) VALUES (?, ?)'
+        self.cursor.execute(stmt, (str(part_name), str(part_number)))
+        self.db.database.commit()
