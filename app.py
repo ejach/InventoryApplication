fe2e0058612ebe97b8ec7eff1f6fb3f30d53537b
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-26 15:37:22 -0400

    Created DatabaseConnector/Manipulator class / Code refactoring

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-from db_manipulator import fetchall
+from src.DatabaseManipulator import DatabaseManipulator
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = fetchall()
+    dbm = DatabaseManipulator()
+    results = dbm.fetchall()
     return render_template('index.html', results=results)
