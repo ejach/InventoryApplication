267170036af22e1e25df0482d021cf75c7965466
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-28 19:45:14 -0400

    Added 'DatabaseStatements.py' / Refactoring

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request
+from src.DatabaseConnector import DatabaseConnector
 from src.DatabaseManipulator import DatabaseManipulator
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     dbm = DatabaseManipulator()
-    results, error = dbm.fetchall()
+    dbc = DatabaseConnector()
+    results = dbm.fetchall()
+    host = dbc.get_host()
+    port = dbc.get_port()
     if request.method == 'POST':
         part_name = request.form['partName']
         part_number = request.form['partNumber']
         dbm.insert(part_name=part_name, part_number=part_number)
-    return render_template('index.html', results=results, error=error)
+        return redirect(url_for('index'))
+    return render_template('index.html', results=results, host=host, port=port)
--- static/js/reload.js
+++ static/js/reload.js
@@ -1,5 +1,28 @@
 window.onload = function() {
-    document.getElementById('submit').addEventListener("click", function() {
-        console.log('this works')
+    document.getElementById('refreshBtn').addEventListener("click", function() {
+        const request = new XMLHttpRequest()
+        const url = '/';
+        request.open('GET', url);
+        request.send();
+
+        request.onload = (e) => {
+            console.log(request.response)
+        }
     })
 }
+
+// $("#refreshBtn").keyup(function(){
+//     var text = $(this).val();
+//
+//     $.ajax({
+//       url: "/",
+//       type: "get",
+//       data: {jsdata: text},
+//       success: function(response) {
+//         $("#myDiv").html(response);
+//       },
+//       error: function(xhr) {
+//         //Do Something to handle error
+//       }
+//     });
+// });
