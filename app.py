579bc820c7be6122ae6dc8bdafba652863cc998f
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-29 22:45:13 -0400

    Sanitized inputs server-side / Code refactoring on `unit_test.py`

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, escape
 
 from src.DatabaseConnector import DatabaseConnector
 from src.DatabaseManipulator import DatabaseManipulator
@@ -14,8 +14,8 @@ def index():
     host = dbc.get_host()
     port = dbc.get_port()
     if request.method == 'POST':
-        part_name = request.form['partName']
-        part_number = request.form['partNumber']
+        part_name = escape(request.form['partName'])
+        part_number = escape(request.form['partNumber'])
         dbm.insert(part_name=part_name, part_number=part_number)
         return redirect(url_for('index'))
     return render_template('index.html', results=results, host=host, port=port)
