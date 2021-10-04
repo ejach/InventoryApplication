2a3110651fab4981840e7851c32c22ff12b3e917
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-04 17:06:46 -0400

    US 2 - Added `webui_host` env variable / Code refactoring

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def index():
     results = dbm.fetchall()
     host = dbc.get_host()
     port = dbc.get_port()
+    webui_host = dbc.get_webui_host()
     if request.method == 'POST':
         # Sanitizes the input using escape
         part_name = clean(request.form['partName'])
@@ -22,7 +23,7 @@ def index():
         dbm.insert(part_name=part_name, part_number=part_number)
         # Redirect when finished
         return redirect(url_for('index'))
-    return render_template('index.html', results=results, host=host, port=port)
+    return render_template('index.html', results=results, webui_host=host, host=host, port=port)
 
 
 # Display the database in JSON format
--- src/DatabaseConnector.py
+++ src/DatabaseConnector.py
@@ -10,6 +10,7 @@ class DatabaseConnector:
         self.db_port = int(environ.get('db_port'))
         self.user = environ.get('username')
         self.password = environ.get('password')
+        self.webui_host = environ.get('webui_host')
         self.db = environ.get('db')
         self.conn = connect(host=self.host, user=self.user, password=self.password, database=self.db,
                             port=self.db_port)
@@ -21,26 +22,17 @@ class DatabaseConnector:
     def get_host(self):
         return self.host
 
-    def set_port(self, port):
-        self.port = port
-
     def get_port(self):
         return self.port
-
-    def set_cursor(self, cursor):
-        self.cursor = cursor
 
     def get_cursor(self):
         return self.cursor
 
-    def set_password(self, password):
-        self.password = password
-
     def get_password(self):
         return self.password
 
-    def set_conn(self, conn):
-        self.conn = conn
+    def get_webui_host(self):
+        return self.webui_host
 
     # Test if the connection is successful; print errors if it was not
     def get_conn(self):
