3356aeb360cdbded2c9db691e7121894f144736e
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-07 18:22:37 -0400

    US 2 - Changed to POST request using JS / Added `htmx` library for dynamically updating page

--- static/js/reload.js
+++ static/js/reload.js
@@ -1,12 +1,33 @@
-window.onload = function() {
-    document.getElementById('refreshBtn').addEventListener("click", function() {
-        const request = new XMLHttpRequest()
-        const url = '/';
-        request.open('GET', url);
-        request.send();
+// This excerpt of code was provided by the Mozilla foundation:
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
+window.addEventListener("load",function() {
+  // Get the myForm element and assign it to a variable
+  const form = document.getElementById("myForm");
 
-        request.onload = (e) => {
-            console.log(request.response)
-        }
-    })
-}
+  function sendData() {
+    const XHR = new XMLHttpRequest();
+
+    // Bind the FormData object and the form element
+    const FD = new FormData(form);
+
+    // Define what happens in case of error
+    XHR.addEventListener("error", function( event) {
+      console.log('POST request was not successful.');
+    });
+
+    // Set up our request
+    XHR.open("POST","/");
+
+    // The data sent is what the user provided in the form
+    XHR.send(FD);
+
+    // Reset the form
+    document.getElementById('myForm').reset()
+  }
+
+  // Replace the submit event with our sendData function
+  form.addEventListener( "submit", function ( event ) {
+    event.preventDefault();
+    sendData();
+  });
+});
