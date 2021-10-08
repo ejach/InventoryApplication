53cfd98c1501d604b7038a5e988a4a6f0b9a9372
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-08 15:50:14 -0400

    US 2 - Revamped UI / Optimized for Mobile / Fine-tuned "hamburger menu"

--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,6 +1,5 @@
-// Function to show/hide the contact form
 $(document).ready(function() {
   $(".container").click(function() {
-    $("#sidenav").slideToggle(200);
+    $("#sidenav").slideToggle(300, "swing");
   });
 });
