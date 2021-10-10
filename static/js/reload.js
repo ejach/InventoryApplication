7eb00ab2392eb57e8b716ae55b45725c1f2a9491
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-10 13:05:54 -0400

    US 2 - Refactored `reload.js` / Implemented PTR library

--- static/js/reload.js
+++ static/js/reload.js
@@ -9,6 +9,11 @@ window.addEventListener("load",function() {
     // Bind the FormData object and the form element
     const FD = new FormData(form);
 
+    // On a successful submission, it gets an AJAX request to refresh the contents of the table
+    XHR.addEventListener( "load", function(event) {
+      htmx.ajax('GET', '/table', '#table');
+    } );
+
     // Define what happens in case of error
     XHR.addEventListener("error", function( event) {
       console.log('POST request was not successful.');
@@ -31,8 +36,16 @@ window.addEventListener("load",function() {
   });
 });
 
-// Function to make the 'hamburger' menu animation fire upon clicking
+
 window.onload = function() {
+  // Implementation to use the PullToRefresh library to perform an AJAX request to update the table
+  const ptr = PullToRefresh.init({
+      mainElement: 'body',
+      onRefresh() {
+        htmx.ajax('GET', '/table', '#table');
+      }
+    });
+  // Implementation to make the 'hamburger' menu animation fire upon clicking
   document.getElementById("container").addEventListener("click", function() {
       let x = document.getElementById('container');
       x.classList.toggle("change");
