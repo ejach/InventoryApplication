6730c0618f28830e0213bb02b0f049cb886a53c1
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-10 20:51:19 -0400

    US 2 - Added DELETE functionality / Code refactoring

--- src/DatabaseStatements.py
+++ src/DatabaseStatements.py
@@ -3,9 +3,13 @@ class DatabaseStatements:
     def __init__(self):
         self.select = 'SELECT * FROM parts.parts'
         self.insert = 'INSERT INTO parts.parts (name, part_number) VALUES (%s, %s)'
+        self.delete = 'DELETE FROM parts.parts WHERE `id` = %s'
 
     def get_select_stmt(self):
         return self.select
 
     def get_insert_statement(self):
         return self.insert
+
+    def get_delete_statement(self):
+        return self.delete
