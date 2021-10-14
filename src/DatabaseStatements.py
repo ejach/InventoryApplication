32d850c8b5622e930c86e5a64fe8868238cad223
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-14 11:02:15 -0400

    US 2 - Added `van_number` to database schema / Code refactoring

--- src/DatabaseStatements.py
+++ src/DatabaseStatements.py
@@ -2,12 +2,16 @@ class DatabaseStatements:
 
     def __init__(self):
         self.select = 'SELECT * FROM parts.parts'
-        self.insert = 'INSERT INTO parts.parts (name, part_number) VALUES (%s, %s)'
+        self.select_vans = 'SELECT * FROM parts.parts WHERE `van_number` = %s'
+        self.insert = 'INSERT INTO parts.parts (name, part_number, van_number) VALUES (%s, %s, %s)'
         self.delete = 'DELETE FROM parts.parts WHERE `id` = %s'
-        self.update = 'UPDATE parts.parts set `name` = %s, `part_number` = %s WHERE `id` = %s'
+        self.update = 'UPDATE parts.parts set `name` = %s, `part_number` = %s, `van_number` = %s WHERE `id` = %s'
 
-    def get_select_stmt(self):
+    def get_select_statement(self):
         return self.select
+
+    def get_select_vans_statement(self):
+        return self.select_vans
 
     def get_insert_statement(self):
         return self.insert
