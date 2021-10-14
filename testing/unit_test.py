499e495f1057ef7436f51ae1f45b644cab4d3455
poopi67 <42357644+ejach@users.noreply.github.com>
2021-10-14 12:46:03 -0400

    US 2 - Added unit tests / Refactoring

--- testing/unit_test.py
+++ testing/unit_test.py
@@ -1,31 +1,14 @@
-import sqlite3
-from json import loads
-from os import path
+from http.client import responses
 from unittest import TestCase, main
-from unittest.mock import MagicMock
 
-from dotenv import load_dotenv
+from responses import GET, activate, add
 from requests import get
 
 from src.DatabaseConnector import DatabaseConnector
 
-load_dotenv()
-
 dbc = DatabaseConnector()
-
 host = dbc.get_host()
 port = dbc.get_port()
-database_file = dbc.get_database_file()
-database = dbc.get_database()
-
-
-def is_json(my_json):
-    try:
-        loads(my_json)
-    except ValueError as e:
-        print(e)
-        return False
-    return True
 
 
 class TestApplication(TestCase):
@@ -39,24 +22,29 @@ class TestApplication(TestCase):
 
     # Test the status of the WSGI web server
     def test_connection(self):
-        r = get(f'http://{host}:{port}')
+        r = get(f'http://localhost:5000')
         self.assertTrue(r.status_code == 200)
         print('HTTP 200 test -> PASSED' + '\n')
+        pass
 
-    def test_sqlite_conn(self):
-        self.assertTrue(path.exists(f'../{database_file}'))
-        print('SQLite3 file exists -> PASSED' + '\n')
-        sqlite3.connect = MagicMock(return_value='Connection is Successful')
-        tdbc = TestDatabaseClass()
-        sqlite3.connect.assert_called_with('database.db')
-        self.assertEqual(tdbc.connection, 'Connection is Successful')
-        print('SQLite3 test -> PASSED' + '\n')
+    def test_mysql_conn(self):
+        self.assertTrue(dbc.conn.is_connected())
 
-
-class TestDatabaseClass:
-
-    def __init__(self, connection_string='database.db'):
-        self.connection = sqlite3.connect(connection_string)
+    @activate
+    def testHTTP404(self):
+        add(**{
+            'method': GET,
+            'url': f'http://{host}:{port}',
+            'body': '{"error": "reason"}',
+            'status': 404,
+            'content_type': 'application/json',
+            'adding_headers': {'X-Foo': 'Bar'}
+        })
+        response = get(f'http://{host}:{port}')
+        self.assertEqual({'error': 'reason'}, response.json())
+        print('HTTP Request test -> PASSED' + '\n')
+        self.assertEqual(404, response.status_code)
+        print('HTTP 404 test -> PASSED' + '\n')
 
 
 if __name__ == '__main__':
