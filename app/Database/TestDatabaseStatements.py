1bb1440d57e9d2a7567f5028671ded2cc58e1860
poopi67 <42357644+ejach@users.noreply.github.com>
2021-11-11 13:19:22 -0500

    US 2 - Fixed `database_manipulator_unit_test.py` / Refactoring

--- app/Database/TestDatabaseStatements.py
+++ app/Database/TestDatabaseStatements.py
@@ -3,9 +3,14 @@ class TestDatabaseStatements:
     def __init__(self):
         self.get_id_by_username = 'SELECT `id` FROM parts.accounts WHERE `username` = %s'
         self.delete_account_by_id = 'DELETE FROM parts.accounts WHERE id = %s'
+        self.get_last_id = 'SELECT last_insert_id()'
 
     def get_get_id_by_username(self):
         return self.get_id_by_username
 
     def get_delete_account_by_id(self):
         return self.delete_account_by_id
+
+    def get_get_last_id(self):
+        return self.get_last_id
+
