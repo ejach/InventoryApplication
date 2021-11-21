74bcfc0162751a35d767a7301329d1bfb93ad85e
poopi67 <42357644+ejach@users.noreply.github.com>
2021-11-21 16:21:11 -0500

    US 8 - Added more functions to namespace.js

--- app/static/js/ns/namespace.js
+++ app/static/js/ns/namespace.js
@@ -38,6 +38,38 @@
           $(".deleteBtn").prop("disabled", false);
         });
       });
+    },
+    addPart : function ($getPath) {
+      $('#submit').click(function (event) {
+        // Prevents form from submitting
+        event.preventDefault();
+        const form = $('#myForm')[0];
+        const data = new FormData(form);
+        // Append vanNum from URL to the formData object if the current window is not /parts
+        if (window.location.pathname !== '/parts') {
+          let vanNum = window.location.pathname.split("/")[2];
+          data.append('van', vanNum);
+        }
+        $.ajax({
+          type: 'POST',
+          enctype: 'multipart/form-data',
+          url: '/parts',
+          data: data,
+          processData: false,
+          contentType: false,
+          cache: false,
+          timeout: 800000,
+          // On success, load the span from the getPath
+          success: function () {
+            $('#table').load($getPath);
+          },
+          // On failure, print errors
+          error: function (e) {
+            console.log("ERROR : ", e);
+          }
+        });
+        $(form).trigger('reset');
+      });
     }
   };
 
--- app/static/js/van/van-parts-addPart.js
+++ app/static/js/van/van-parts-addPart.js
@@ -1,34 +1,4 @@
-$(document).ready(function() {
-    const currentURL = (window.location.pathname);
+$(function() {
     const getPath = '/table' + window.location.pathname;
-    // Gets the number of the current van selected by the end of the pathname
-    const vanNum = currentURL.split("/")[2];
-    // On submit, execute the following
-    $('#submit').click(function (event) {
-    // Prevents form from submitting
-    event.preventDefault();
-    const form = $('#myForm')[0];
-    const data = new FormData(form);
-    // Append vanNum from URL to the formData object
-    data.append('van', vanNum);
-      $.ajax({
-          type: 'POST',
-          enctype: 'multipart/form-data',
-          url: '/parts',
-          data: data,
-          processData: false,
-          contentType: false,
-          cache: false,
-          timeout: 800000,
-          // On success, load the span from the getPath
-          success: function () {
-              $('#table').load(getPath);
-          },
-          // On failure, print errors
-          error: function (e) {
-              console.log("ERROR : ", e);
-          }
-      });
-      $(form).trigger('reset');
-  });
+    ns.addPart(getPath);
 });
