c4b00920fb9dec593723720f3ababd7bbe045cd2
Evan <ejach2000@gmail.com>
2021-12-22 17:48:33 -0500

    Implemented `WTForms` using `Flask-WTF` / Refactoring

---
+++ app/Forms/LoginForm.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
+    submit = SubmitField('Submit', id='btnLogin')
--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,32 +1,41 @@
 $(function() {
+    let username = $('#username');
+    let password = $('#password');
+    let instructions = $('#instructions');
+    let btnLogin = $('#btnLogin');
     // On submit, execute the following
-    $("#btnLogin").click(function (event) {
-        // Prevents form from submitting
-        event.preventDefault();
-        const form = $('#loginForm')[0];
-        const data = new FormData(form);
-        // Disable submit button until something happens
-        $("#btnLogin").prop("disabled", true);
-          $.ajax({
-              type: 'POST',
-              enctype: 'multipart/form-data',
-              url: '/login',
-              data: data,
-              processData: false,
-              contentType: false,
-              cache: false,
-              timeout: 800000,
-              // On success, load the span from the getPath and re-enable the submit button
-              success: function () {
-                  $("#btnLogin").prop("disabled", false);
-                  location.reload();
-              },
-              // On failure, print errors and re-enable the submit button
-              error: function (e) {
-                  console.log("ERROR : ", e);
-                  $("#btnLogin").prop("disabled", false);
-              }
-          });
-          $(form).trigger('reset');
+    $(btnLogin).click(function (event) {
+        if (!username.val() || !password.val()) {
+            $(instructions).html('Blank input will not be accepted.').css('color', 'red');
+        } else {
+            // Prevents form from submitting
+            event.preventDefault();
+            const form = $('#loginForm')[0];
+            const data = new FormData(form);
+            // Disable submit button until something happens
+            $(btnLogin).prop("disabled", true);
+            $.ajax({
+                type: 'POST',
+                enctype: 'multipart/form-data',
+                url: '/login',
+                data: data,
+                processData: false,
+                contentType: false,
+                cache: false,
+                timeout: 800000,
+                // On success, load the span from the getPath and re-enable the submit button
+                success: function () {
+                    $(btnLogin).prop("disabled", false);
+                    $(instructions).html('Login').css('color', 'black');
+                    location.reload();
+                },
+                // On failure, print errors and re-enable the submit button
+                error: function (e) {
+                    console.log('ERROR : ', e);
+                    $(btnLogin).prop('disabled', false);
+                }
+            });
+            $(form).trigger('reset');
+        }
       });
 });
