0146ffcf5afdd283456ae0cc2f0213f5acbe40bf
Evan J <ejach2000@gmail.com>
2022-02-01 11:40:01 -0500

    Refactored database.sql

--- database.sql
+++ database.sql
@@ -1,23 +1,32 @@
-# Used for the parts database
+-- Used for the parts database
 create table parts
 (
-	id int auto_increment,
-	name text null,
-	amount int default 0 null,
-	part_number text null,
-	van_number text null,
-	constraint parts_pk
-		primary key (id)
+    id          int auto_increment
+        primary key,
+    name        varchar(255)             null,
+    amount      int          default 0   not null,
+    part_number varchar(255) default '0' null,
+    van_number  varchar(255)             null,
+    constraint parts_fk
+        foreign key (van_number) references vans (van_number)
 );
 
-# Table for the vans
+create index van_number
+    on parts (van_number);
+
+
+
+-- Table for the vans
 create table vans
 (
-	id int auto_increment,
-	van_number text null,
-	constraint vans_pk
-		primary key (id)
+    id         int auto_increment
+        primary key,
+    van_number varchar(255) null
 );
+
+create index van_number
+    on vans (van_number);
+
 
 # Table for accounts
 create table accounts
