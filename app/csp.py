f35d6a7ad1c4dc1828c2ea4c416a391ec0190644
Evan J <ejach2000@gmail.com>
2022-03-15 18:25:12 -0400

    Refactored `csp.py`

--- app/csp.py
+++ app/csp.py
@@ -1,6 +1,7 @@
 csp = {
     'img-src': '*',
     'object-src': '\'none\'',
+    'default-src': '\'self\'',
     'script-src': [
         '\'self\'',
         'ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'
